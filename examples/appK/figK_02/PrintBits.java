90324c70ad86d8e5029a1c9c844246ec9c8f233c
chicagoist <chicagoist@exploit.im>
2024-01-05 00:57:58 +0100

    some fixes to Modules

--- examples/appK/figK_02/PrintBits.java
+++ examples/appK/figK_02/PrintBits.java
@@ -3,32 +3,31 @@
 import java.util.Scanner;
 
 public class PrintBits {
-   public static void main(String[] args) {
-      // get input integer
-      Scanner scanner = new Scanner(System.in);
-      System.out.println("Please enter an integer:");
-      int input = scanner.nextInt();
+  public static void main(String[] args) {
+    // get input integer
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please enter an integer:");
+    int input = scanner.nextInt();
 
-      // display bit representation of an integer
-      System.out.println("\nThe integer in bits is:");
+    // display bit representation of an integer
+    System.out.println("\nThe integer in bits is:");
 
-      // create int value with 1 in leftmost bit and 0s elsewhere
-      int displayMask = 1 << 31;                                 
+    // create int value with 1 in leftmost bit and 0s elsewhere
+    int displayMask = 1 << 31;
 
-      // for each bit display 0 or 1 
-      for (int bit = 1; bit <= 32; bit++) {
-         // use displayMask to isolate bit
-         System.out.print((input & displayMask) == 0 ? '0' : '1');
+    // for each bit display 0 or 1
+    for (int bit = 1; bit <= 32; bit++) {
+      // use displayMask to isolate bit
+      System.out.print((input & displayMask) == 0 ? '0' : '1');
 
-         input <<= 1; // shift value one position to left 
+      input <<= 1; // shift value one position to left
 
-         if (bit % 8 == 0) {
-            System.out.print(' '); // display space every 8 bits
-         }
+      if (bit % 8 == 0) {
+        System.out.print(' '); // display space every 8 bits
       }
-   }
-} 
-
+    }
+  }
+}
 
 /**************************************************************************
  * (C) Copyright 1992-2018 by Deitel & Associates, Inc. and               *
